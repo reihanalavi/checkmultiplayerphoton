920c1eeae9ee51de8eddb9a7aa4fd0ad5e3b409f
reihanalavi <reihanalavi@users.noreply.github.com>
2021-05-29 18:04:51 +0700

    First Commit
    First try

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1622283994,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player_actor-sheet0.png",
+		"images/player_actor-sheet1.png",
+		"images/bullet-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
